3ef3818611078334bf188a39094e16c15d88a633
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-08-07 14:01:00 +0300

    task in process

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import './css/styles.css';
 import API from './fetchCountries';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 const inputCountry = document.querySelector('input');
 const list = document.querySelector(".country-list");
 const countryInfo = document.querySelector(".country-info");
+
+list.style.listStyleType = "none";
+
 
 inputCountry.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
 
@@ -14,27 +18,56 @@ function searchCountry(e) {
     e.preventDefault();
     
     const countryName = inputCountry.value;
-    
-    
+    if (countryName === "") {
+        return
+    }
+    if (countryName.length > 10) {
+        Notiflix.Notify.failure("Please enter a more specific name.");
+    }
+    else {
     API.fetchCountries(countryName)
     .then((data) => insertContent(data))
-    .catch(error => {
-       console.log(error)
-    })    
+    .catch(error => {        
+        Notiflix.Notify.failure(error);
+    })  
+    } 
 }
-const createListItem = (item) =>
-    `<li>    
-    <img src="${item.flags.svg}" alt="${item.name.official}" width="50px">
-    <h2>${item.name.official ? item.name.official : ""}</h2>     
-    <p>Population: ${item.population ? item.population : ""}</p>
-    <p>Capital: ${item.capital ? item.capital : ""}</p>
-    <p>Official language(s) in country: ${item.languages.value ? item.languages.value : ""}</p>    
+
+
+function createListItem(item) {
+    
+    const createEl = `<li>
+    <table>
+     <tbody>
+        <tr>
+        <td><img src="${item.flags.svg}" alt="${item.name.official}" width="20px"></td>
+        <td><h2>${Object.values(item.name.common).join('')}</h2></td>
+        </tr>        
+     </tbody>          
+    </table> 
+    <p><b>Capital:</b> ${item.capital ? item.capital : ""}</p>
+        <p><b>Population:</b> ${item.population ? item.population : ""}</p>
+        <p><b>Language(s):</b> ${Object.keys(item.languages) ? Object.keys(item.languages) : ""}</p>       
     </li>`;
+    return createEl;
+}
 
-const generateContent = (array) => array?.reduce((acc, item) => acc + createListItem(item), "");
-const insertContent = (array) => {
-    const result = generateContent(array);
-    countryInfo.insertAdjacentHTML("beforeend", result);
-} 
+// function createCountryInfo(item) { 
+//     return  `<p><b>Capital:</b> ${item.capital ? item.capital : ""}</p>
+//         <p><b>Population:</b> ${item.population ? item.population : ""}</p>
+//         <p><b>Language(s):</b> ${Object.keys(item.languages) ? Object.keys(item.languages) : ""}</p>`
+// }
+     
+        
 
+const generateCountry = (array) => array?.reduce((acc, item) => acc + createListItem(item), "");
 
+// const generateInfo = (array) => array?.reduce((acc, item) => acc + createCountryInfo(item), "");
+
+const insertContent = (array) => {    
+    const result = generateCountry(array);
+    list.insertAdjacentHTML("beforeend", result);
+
+    // const info = generateInfo(array)    
+    // countryInfo.insertAdjacentHTML("beforeend", info);
+}
