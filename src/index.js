f1566aca4881cbcdd88e1bb4d8b495c8f4bf4556
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-08-09 20:15:25 +0300

    add html for one  country

--- src/index.js
+++ src/index.js
@@ -31,8 +31,13 @@ function searchCountry(e) {
                 Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
                 list.innerHTML = "";
             }
+            
+            if (data.length === 1) {    
+                ;
+                return insertInfo(data);
+            }
             else {
-                return insertContent(data)
+                return insertContent(data);
             }
             
         })
@@ -41,6 +46,7 @@ function searchCountry(e) {
     })  
     } 
 }
+
 
 
 function createListItem(item) {
@@ -53,30 +59,37 @@ function createListItem(item) {
         <td><h2>${Object.values(item.name.common).join('')}</h2></td>
         </tr>        
      </tbody>          
-    </table> 
-    <p><b>Capital:</b> ${item.capital ? item.capital : ""}</p>
-        <p><b>Population:</b> ${item.population ? item.population : ""}</p>
-        <p><b>Language(s):</b> ${Object.keys(item.languages) ? Object.keys(item.languages) : ""}</p>       
+    </table>     
     </li>`;
     return createEl;
 }
 
-// function createCountryInfo(item) { 
-//     return  `<p><b>Capital:</b> ${item.capital ? item.capital : ""}</p>
-//         <p><b>Population:</b> ${item.population ? item.population : ""}</p>
-//         <p><b>Language(s):</b> ${Object.keys(item.languages) ? Object.keys(item.languages) : ""}</p>`
-// }
+function createCountryInfo(item) { 
+    return  ` <table>
+     <tbody>
+        <tr>
+        <td><img src="${item.flags.svg}" alt="${item.name.official}" width="20px"></td>
+        <td><h2>${Object.values(item.name.common).join('')}</h2></td>
+        </tr>        
+     </tbody>          
+    </table> 
+    <p><b>Capital:</b> ${item.capital ? item.capital : ""}</p>
+        <p><b>Population:</b> ${item.population ? item.population : ""}</p>
+        <p><b>Language(s):</b> ${Object.keys(item.languages) ? Object.keys(item.languages) : ""}</p>`
+}
      
         
 
 const generateCountry = (array) => array?.reduce((acc, item) => acc + createListItem(item), "");
 
-// const generateInfo = (array) => array?.reduce((acc, item) => acc + createCountryInfo(item), "");
+const generateInfo = (array) => array?.reduce((acc, item) => acc + createCountryInfo(item), "");
 
-const insertContent = (array) => {    
+const insertContent = (array) => {
     const result = generateCountry(array);
     list.insertAdjacentHTML("beforeend", result);
+}
 
-    // const info = generateInfo(array)    
-    // countryInfo.insertAdjacentHTML("beforeend", info);
+const insertInfo = (array) => {
+    const result = generateInfo(array);
+    countryInfo.insertAdjacentHTML("beforeend", result)
 }
